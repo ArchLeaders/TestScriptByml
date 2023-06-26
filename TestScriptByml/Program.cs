37e98cf0115bbf34627b50dc2f3f0bc280b742ae
ArchLeaders <archparliament@gmail.com>
2023-06-25 18:38:18 -0700

    Add source files

--- TestScriptByml/Program.cs
+++ TestScriptByml/Program.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using Cead;
+using Cead.Handles;
+using Cead.Interop;
+
+DllManager.LoadCead();
+
+string path = args[0];
+
+using FileStream fs = File.OpenRead(path);
+Span<byte> buffer = new byte[fs.Length];
+fs.Read(buffer);
+
+Byml byml = Byml.FromBinary(buffer);
+Byml.Array array = byml.GetHash()["PathList"].GetArray();
+
+array.Add("Work/Actor/");
+array.Add(args[1]);
+array.Add(".engine__actor__ActorParam.gyml");
+
+using DataHandle data = byml.ToBinary(false, 7);
+using FileStream writer = File.Create(args[2]);
+writer.Write(data);
